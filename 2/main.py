2e31821587433c7789582fe4d80f2b3a7e74e5f2
Julián <jr@okapii.com.ar>
2025-09-22 03:04:27 -0300

    Implemento las funciones que hay que hacer del modulo, habia un test mal hecho por la catedra, no me fije si quedaba jugar con algo o alguna transformacion

--- 2/main.py
+++ 2/main.py
@@ -48,14 +48,137 @@ def grid_plot(ax, ab, limits, a_label, b_label):
            xlim=limits[0], ylim=limits[1],
            xlabel=a_label, ylabel=b_label)
 
+def rota(theta):
+    """
+    Recibe un angulo theta y retorna una matriz de 2x2
+    que rota un vector dado en un angulo theta
+    """
+    res =np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta),  np.cos(theta)]
+    ])
+    return res
+
+def escala(s):
+    """
+    Recibe una tira de numeros s y retorna una matriz cuadrada de
+    n x n, donde n es el tamaño de s.
+    La matriz escala la componente i de un vector de Rn
+    en un factor s[i]
+    """
+    res =np.diag(s)
+    return res
+
+def rota_y_escala(theta,s):
+    """
+    Recibe un angulo theta y una tira de numeros s,
+    y retorna una matriz de 2x2 que rota el vector en un angulo theta
+    y luego lo escala en un factor s
+    """
+    res = rota(theta)@escala(s)
+    return res
+
+def afin(theta,s,b):
+    """
+    Recibe un angulo theta , una tira de numeros s (en R2) , y un vector
+    b en R2.
+    Retorna una matriz de 3x3 que rota el vector en un angulo theta,
+    luego lo escala en un factor s y por ultimo lo mueve en un valor
+    fijo b
+    """
+    matriz2x2 = rota_y_escala(theta,s)
+    matriz3x3 = np.zeros((3, 3))
+    matriz3x3[:2, :2] = matriz2x2
+    matriz3x3[0][2] = b[0]
+    matriz3x3[1][2] = b[1]
+    matriz3x3[2][2] = 1
+    return matriz3x3
+
+def trans_afin(v,theta,s,b):
+    """
+    Recibe un vector v (en R2), un angulo theta,
+    una tira de numeros s (en R2), y un vector b en R2.
+    Retorna el vector w resultante de aplicar la transformacion afin a
+    v
+    """
+    transf = afin(theta,s,b)
+
+    vectorCon1 =  np.append(v, 1) 
+    vectorColumna = vectorCon1.T
+    vectorColumnaRes = transf@vectorColumna
+    res = vectorColumnaRes.T[:2]
+    return res
+
 def main():
-    print('Ejecutar el programa')
+    #print('Ejecutar el programa')
     # generar el tipo de transformacion dando valores a la matriz T
-    T = pd.read_csv('T.csv', header=None).values
-    corners = np.array([[0,0],[100,100]])
+    #T = pd.read_csv('T.csv', header=None).values
+    #corners = np.array([[0,0],[100,100]])
     # corners = np.array([[-100,-100],[100,100]]) array con valores positivos y negativos
-    wz = pointsGrid(corners)
-    vistform(T, wz, 'Deformar coordenadas')
+    #wz = pointsGrid(corners)
+    #vistform(T, wz, 'Deformar coordenadas')
+
+    # Tests para rota
+    assert(np.allclose(rota(0), np.eye(2)))
+    assert(np.allclose(rota(np.pi/2), np.array([[0, -1],[1, 0]])))
+    assert(np.allclose(rota(np.pi), np.array([[-1, 0],[0, -1]])))
+
+    # Tests para escala
+    assert(np.allclose(escala([2,3]), np.array([[2,0],[0,3]])))
+    assert(np.allclose(escala([1,1,1]), np.eye(3)))
+    assert(
+        np.allclose(escala([0.5,0.25]), np.array([[0.5,0],[0,0.25]]))
+    )
+
+    # Tests para rota-y-escala
+    assert(
+        np.allclose(rota_y_escala(0,[2,3]), np.array([[2,0],[0,3]]))
+    )
+    assert(np.allclose(
+        rota_y_escala(np.pi/2,[1,1]), np.array([[0,-1],[1,0]])
+    ))
+    assert(np.allclose(
+        rota_y_escala(np.pi,[2,2]), np.array([[-2,0],[0,-2]]))
+    )
+
+    # Tests para afin
+    assert(np.allclose(
+        afin(0,[1,1],[1,2]),
+        np.array([[1,0,1],
+                [0,1,2],
+                [0,0,1]]))
+    )
+
+    assert(np.allclose(afin(np.pi/2,[1,1],[0,0]),
+        np.array([[0,-1,0],
+                [1,0,0],
+                [0,0,1]]))
+    )
+
+    assert(np.allclose(afin(0,[2,3],[1,1]),
+        np.array([[2,0,1],
+                [0,3,1],
+                [0,0,1]]))
+    )
+
+    # Tests para trans_afin
+    assert(np.allclose(
+        trans_afin(np.array([1,0]), np.pi/2,[1,1],[0,0]),
+        np.array([0,1])
+    ))
+
+    assert(np.allclose(
+        trans_afin(np.array([1,1]), 0,[2,3],[0,0]),
+        np.array([2,3])
+    ))
+
+    assert(np.allclose(
+        trans_afin(np.array([1,0]), np.pi/2,[3,2],[4,5]),
+        #np.array([4,7])  #EL TEST ESTA MAL HECHO DEBERIA DAR [4,8]
+        np.array([4,8])
+    ))
+
+        
     
     
 if __name__ == "__main__":
